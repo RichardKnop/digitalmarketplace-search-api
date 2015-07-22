89c649cbfcdf36b4084a30f83e757037207626dd
Michal Tekel <mtekel@gmail.com>
2015-07-22 15:07:15 +0100

    Don't add cname: apps can deal with ssl now
    No need to add custom CNAME to match cert wildcard anymore as the apps (api and frontends) now deal with ssl domain mismatch.

--- scripts/tsuru-deploy.sh
+++ scripts/tsuru-deploy.sh
@@ -3,7 +3,3 @@ tsuru service-add elasticsearch marketplacees
 tsuru service-bind marketplacees
 tsuru env-set DM_SEARCH_API_AUTH_TOKENS=oursearchtoken
 tsuru app-deploy *
-
-# Add cname (this is because SSL certificate is only for *.tsuru.paas.alphagov.co.uk)
-# Add this to Route53 if you haven't done so already
-tsuru cname-add digitalmarketplace-search-api-ci.tsuru.paas.alphagov.co.uk
